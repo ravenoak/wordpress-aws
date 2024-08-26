07256410a71e8d372de7d0a8e123c35088aaada8
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2024-08-25 18:03:40 -0700

    Fixes some networking elements

--- deployments/subnets.tf
+++ deployments/subnets.tf
@@ -60,6 +60,15 @@ resource "aws_nat_gateway" "nat_2" {
   subnet_id     = aws_subnet.public_2.id
 }
 
+resource "aws_route_table" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
 resource "aws_route_table" "private_1" {
   vpc_id = aws_vpc.main.id
 
@@ -76,6 +85,16 @@ resource "aws_route_table" "private_2" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_2.id
   }
+}
+
+resource "aws_route_table_association" "public_1_igw" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.igw.id
+}
+
+resource "aws_route_table_association" "public_2_igw" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.igw.id
 }
 
 resource "aws_route_table_association" "reverse_proxy_private_1" {
