a25aa6978ec3d4ca45cc20758182e3612b0891a1
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2024-08-25 18:35:43 -0700

    fmt

--- deployments/security_groups.tf
+++ deployments/security_groups.tf
@@ -4,16 +4,16 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -24,16 +24,16 @@ resource "aws_security_group" "reverse_proxy_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -44,16 +44,16 @@ resource "aws_security_group" "wordpress_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 9000
-    to_port   = 9000
-    protocol  = "tcp"
+    from_port       = 9000
+    to_port         = 9000
+    protocol        = "tcp"
     security_groups = [aws_security_group.reverse_proxy_sg.id]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -63,16 +63,16 @@ resource "aws_security_group" "efs_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 2049
-    to_port   = 2049
-    protocol  = "tcp"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -82,16 +82,16 @@ resource "aws_security_group" "aurora_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 3306
-    to_port   = 3306
-    protocol  = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
