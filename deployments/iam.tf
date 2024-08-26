ab0b6eb1ae5381da1fd9945be23ed2d634c48d7d
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2024-08-25 19:37:11 -0700

    Updates ECS role permissions

--- deployments/iam.tf
+++ deployments/iam.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "ecs_task_execution_role" {
+data "aws_iam_policy_document" "ecs_task_execution_role_trust" {
   statement {
     actions = [
       "sts:AssumeRole",
@@ -11,7 +11,23 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
   }
 }
 
+data "aws_iam_policy_document" "ecs_task_execution_role_permissions" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "ecs_task_execution_role" {
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role_trust.json
   name               = "ecs-task-execution-role"
+
+  inline_policy {
+    name   = "ecs-task-execution-role-policy"
+    policy = data.aws_iam_policy_document.ecs_task_execution_role_permissions.json
+  }
 }
